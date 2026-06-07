bc9e5ea368bf2a5415803f6b54eee5e35c8cc0d2
nathandebarros <nathanbartolomeu@gmail.com>
2026-06-07 20:18:56 -0300

    Update Editor de Texto.py
    Código mais bizarro já visto na história da humanidade, pelo menos é um começo

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+texto = 'Você sabia que sábia, sabia assobiar?'
+
+#Declarar uma váriavel responsável pela ação
+acao, sub_acao = input("Qual a ação desejada: ").split()
+
+c = 0
+while acao != 'F': 
+
+    if acao == 'E':
+        n = int(sub_acao)
+        if sub_acao > len(texto):
+            n = len(texto)
+        c = n 
+        print(texto[c])
+    
+
+    if acao == 'D':
+        n = int(sub_acao)
+        if n > len(texto) - c:
+            n = len(texto) - c
+        c = n
+        print(texto[c])
+        
+    if acao == 'x':
+        texto = texto.replace(texto[c] , '')
+        print(texto)
+
+
+    acao , sub_acao = input("Qual a ação desejada: ").split()
