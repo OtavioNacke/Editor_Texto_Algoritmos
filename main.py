12bd581e68ee52b5a185594ef1b67ac640830560
Otávio Nacke <otaviopaulino1@gmail.com>
2026-06-26 15:24:13 -0300

    funções incompletas

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-texto = 'Você sabia que sábia, sabia assobiar?'
+texto = input()
 
-#Declarar uma váriavel responsável pela ação
-acao, sub_acao = input("Qual a ação desejada: ").split()
+cursor = 0
+while True:
+    comando = input().split() #divide o input no comando e no argumento
 
-c = 0
-while acao != 'F': 
-
-    if acao == 'E':
-        n = int(sub_acao)
-        if sub_acao > len(texto):
-            n = len(texto)
-        c = n 
-        print(texto[c])
+    if comando[0] == 'F': #quebra tudo
+        break
     
-
-    if acao == 'D':
-        n = int(sub_acao)
-        if n > len(texto) - c:
-            n = len(texto) - c
-        c = n
-        print(texto[c])
+    if comando[0] == 'E':
+        n = int(comando[1]) #pega apenas o argumento, o número e converte pra int
+        cursor -= n         #move o cursor para a esquerda a partir da posição atual do cursor
+        if cursor < 0:      #se o cursor for menor que 0, ele vai pro início
+            cursor = 0
         
-    if acao == 'x':
-        texto = texto.replace(texto[c] , '')
-        print(texto)
+    elif comando[0] == 'D':
+        n = int(comando[1]) 
+        cursor += n         
+        if cursor > len(texto): #se o cursor for maior que o comprimento do texto, ele vai pro final
+            cursor = len(texto)
+    
+    elif comando[0] == 'i':
+        char = comando[1]       #pega o argumento, que é o caractere
+        texto = texto[:cursor] + char + texto[cursor:]   #aq ele pega o texto antes do cursor [:cursor], coloca o char e depois pega o texto depois do cursor [cursor:]
+
+        cursor += 1             #pra ele andar um pra direita depois de colocar o char
+
+    elif comando[0] == 'I':
+        palavra = comando[1] #pega a palavra
+        if cursor != 0:
+            texto = texto[:cursor] + ' ' + palavra + texto[cursor:]  #coloca a palavra com a mesma ideia do de cima, mas coloca um espaço antes
+            cursor += len(palavra) + 1    #aq, é pra andar o cursor até o final da palavra. tem o +1 pq tem o espaço
+        else:
+            texto = palavra + texto  #se o cursor for 0, estiver no inicio, n precisa colocar espaço antes. (eu n sei se a gente coloca espaço depois, faz sentido, mas n ta no enunciado)
+            cursor += len(palavra)
 
 
-    acao , sub_acao = input("Qual a ação desejada: ").split()
+
+print(texto)
+
+
+
